10decc29daf2e2bb46bf5ccd42ae925c1234aab3
Yassir Karroum <ukarroum17@gmail.com>
2022-04-08 21:29:31 +0200

    Added buzzer

--- alarm_system.ino/alarm_system.ino.ino
+++ alarm_system.ino/alarm_system.ino.ino
@@ -2,6 +2,7 @@ void setup() {
   // put your setup code here, to run once:
   pinMode(2, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(9, OUTPUT);
   Serial.begin(9600);
 }
 
@@ -11,7 +12,9 @@ void loop() {
   {
     Serial.println("HIGH");
     digitalWrite(LED_BUILTIN, HIGH);
+    tone(9, 1000);
   }
   else
     digitalWrite(LED_BUILTIN, LOW);
+    noTone(9); 
 }
